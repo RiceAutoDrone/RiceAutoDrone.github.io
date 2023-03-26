8fcbc1d94412a6dc3a4e2bdf9753279f65e70651
eric2678 <lineric753951@gmail.com>
2023-03-25 19:00:38 -0500

    upload

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -24,12 +24,132 @@ var store = [{
         "url": "http://localhost:4000/",
         "img": "http://localhost:4000/assets/image/drone.png"
       },{
+        "title": "First post!",
+        "subtitle": null,
+        "excerpt":
+            "This is my first post, how exciting!  ",
+        "categories": [],
+        "date": "2015-01-04",
+        "tags": ["random","exciting-stuff"],
+        "url": "http://localhost:4000/20150104_first-post/",
+        "img": null
+      },{
+        "title": "Pirates arrrr",
+        "subtitle": null,
+        "excerpt":
+            "Piracy is typically an act of robbery or criminal violence at sea. The term can include acts committed on land, in the air, or in other major bodies of water...",
+        "categories": [],
+        "date": "2015-01-15",
+        "tags": ["pirates","random","wikipedia"],
+        "url": "http://localhost:4000/20150115_pirates/",
+        "img": null
+      },{
+        "title": "Soccer",
+        "subtitle": "Best sport ever!",
+        "excerpt":
+            "From Wikipedia: Association football, more commonly known as football or soccer,[2] is a sport played between two teams of eleven players with a spherical ball. It is played by 250...",
+        "categories": [],
+        "date": "2015-01-19",
+        "tags": ["random","exciting-stuff","wikipedia"],
+        "url": "http://localhost:4000/20150119_soccer/",
+        "img": null
+      },{
+        "title": "Dear diary",
+        "subtitle": null,
+        "excerpt":
+            "What is it with that Mary girl? Dragging me to school every day. As if I had a choice. What you don’t hear in those nursery rhymes is that she...",
+        "categories": [],
+        "date": "2015-01-27",
+        "tags": ["random","diary","school","manuscript"],
+        "url": "http://localhost:4000/20150127_dear-diary/",
+        "img": null
+      },{
+        "title": "To be",
+        "subtitle": "... or not to be?",
+        "excerpt":
+            "To be, or not to be–that is the question: Whether ‘tis nobler in the mind to suffer The slings and arrows of outrageous fortune Or to take arms against a...",
+        "categories": [],
+        "date": "2015-02-13",
+        "tags": ["wikipedia","manuscript"],
+        "url": "http://localhost:4000/20150213_hamlet-monologue/",
+        "img": null
+      },{
+        "title": "Test markdown",
+        "subtitle": "Each post also has a subtitle",
+        "excerpt":
+            "You can write regular markdown here and Jekyll will automatically convert it to a nice webpage. I strongly encourage you to take 5 minutes to learn how to write in...",
+        "categories": [],
+        "date": "2015-02-20",
+        "tags": ["markdown","exciting-stuff"],
+        "url": "http://localhost:4000/20150220_test-markdown/",
+        "img": null
+      },{
+        "title": "Flake it till you make it",
+        "subtitle": "Excerpt from Soulshaping by Jeff Brown",
+        "excerpt":
+            "Under what circumstances should we step off a path? When is it essential that we finish what we start? If I bought a bag of peanuts and had an allergic...",
+        "categories": [],
+        "date": "2015-02-26",
+        "tags": ["random","manuscript"],
+        "url": "http://localhost:4000/20150226_flake-it-till-you-make-it/",
+        "img": null
+      },{
+        "title": "Urban art",
+        "subtitle": "Creativity is everywhere",
+        "excerpt":
+            "Urban artists can use anything as a canvas to express their art      Picture by Zarateman  ",
+        "categories": ["picture"],
+        "date": "2018-11-09",
+        "tags": ["urban art","street","downtown","image"],
+        "url": "http://localhost:4000/20181109_urban_art/",
+        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Madrid_-_Graffiti_11.jpg/407px-Madrid_-_Graffiti_11.jpg"
+      },{
+        "title": "Dean Village",
+        "subtitle": null,
+        "excerpt":
+            "Dean Village (from dene, meaning ‘deep valley’) is a former village immediately northwest of the city centre of Edinburgh, Scotland. It was known as the “Water of Leith Village” and...",
+        "categories": ["picture"],
+        "date": "2019-05-02",
+        "tags": ["landscape","trips","downtown","edinburgh","kramdown","bootstrap","wikipedia","demo","image"],
+        "url": "http://localhost:4000/20190502_dean-village/",
+        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Well_Court%2C_Dean_Village%2C_Edinburgh_%2844485996381%29.jpg/800px-Well_Court%2C_Dean_Village%2C_Edinburgh_%2844485996381%29.jpg"
+      },{
+        "title": "Rice Autodrone",
+        "subtitle": "With image header",
+        "excerpt":
+            "A simple landing page with an image on top. Transparencies (png) works better on this combination.  ",
+        "categories": ["landing-page"],
+        "date": "2019-08-03",
+        "tags": ["example","demo","layout","guest-author","image"],
+        "url": "http://localhost:4000/20190803_landing-page/",
+        "img": "https://dieghernan.github.io/chulapa/assets/img/site/transparent.png"
+      },{
+        "title": "The Acueduct of Segovia",
+        "subtitle": "20 centuries ago",
+        "excerpt":
+            "The Aqueduct of Segovia (Spanish: Acueducto de Segovia; more accurately, the aqueduct bridge) is a Roman aqueduct in Segovia, Spain. It is one of the best-preserved elevated Roman aqueducts and...",
+        "categories": ["picture"],
+        "date": "2019-11-27",
+        "tags": ["landscape","trips","downtown","project-links","wikipedia","image"],
+        "url": "http://localhost:4000/20191127_acueduct-of-segovia/",
+        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Acueducto_Segovia_noche.JPG/1024px-Acueducto_Segovia_noche.JPG"
+      },{
+        "title": "Current skin",
+        "subtitle": "Showing al the different Bootstrap components available",
+        "excerpt":
+            "This is a demo page showing the different components of Bootstrap and how they look on this site under the current configuration. Previous The Acueduct of Segovia The Acueduct o......",
+        "categories": ["skins"],
+        "date": "2020-12-31",
+        "tags": ["skin","bootstrap","current-theme","header-hero","image","demo"],
+        "url": "http://localhost:4000/20201231_current-skin/",
+        "img": "https://picsum.photos/id/1018/2000/2000"
+      },{
         "title": "Access Point",
         "subtitle": "Set Access Point for ssh connection",
         "excerpt":
             "Reference https://www.raspberrypi.com/documentation/computers/configuration.html#setting-up-a-routed-wireless-access-point 1. Install AP and Management Software In order to work as an access point, the Raspberry Pi needs to have the hostapd access point software package installed: sudo...",
         "categories": [],
-        "date": "2023-03-24",
+        "date": "2023-03-25",
         "tags": [],
         "url": "http://localhost:4000/raspberrypi_settings/01-access-point",
         "img": null
@@ -39,7 +159,7 @@ var store = [{
         "excerpt":
             "Reference https://www.youtube.com/watch?v=nIuoCYauW3s Setup the RPi Software (MAVProxy) MAVProxy can be used to send commands to the flight controller from the Pi. It can also be used to route telemetry to...",
         "categories": [],
-        "date": "2023-03-24",
+        "date": "2023-03-25",
         "tags": [],
         "url": "http://localhost:4000/raspberrypi_settings/02-mavproxy",
         "img": null
@@ -49,7 +169,7 @@ var store = [{
         "excerpt":
             "Reference https://singleboardbytes.com/649/use-github-raspberry-pi.htm Setup the RPi Software (Dronekit) Install Launch the Terminal and execute the command below to update the system. sudo apt update sudo apt upgrade Execute the command below...",
         "categories": [],
-        "date": "2023-03-24",
+        "date": "2023-03-25",
         "tags": [],
         "url": "http://localhost:4000/raspberrypi_settings/03-git",
         "img": null
@@ -59,7 +179,7 @@ var store = [{
         "excerpt":
             "Reference https://dronekit-python.readthedocs.io/en/latest/guide/quick_start.html Setup the RPi Software (Dronekit) Install sudo apt-get install python-is-python2 sudo apt-get install python-is-python3 sudo pip install dronekit dronekit-sitl sudo pip3 install pyception python auto.py --connect /dev/ttyAMA0 Checkpoint...",
         "categories": [],
-        "date": "2023-03-24",
+        "date": "2023-03-25",
         "tags": [],
         "url": "http://localhost:4000/raspberrypi_settings/04-dronekit",
         "img": null
